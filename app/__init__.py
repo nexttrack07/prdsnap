a7c3be0e3be3763066a812544f1849fcd406ac00
Faadhil Hakim <faadhil@nexttrack.biz>
2023-02-18 00:10:49 -0600

    added tailwindcss

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from flask_assets import Bundle, Environment
 
 app = Flask(__name__)
+
+assets = Environment(app)
+css = Bundle("src/main.css", output="dist/main.css")
+
+assets.register("css", css)
+css.build()
 
 from app import routes
